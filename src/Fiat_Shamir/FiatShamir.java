f054c4f006fc5791a2d055d644a11eefffddaad4
lexx129 <aleks04.94@gmail.com>
2015-11-07 01:14:43 +0300

    signing and checking completed. It should work now :)

--- src/Fiat_Shamir/FiatShamir.java
+++ src/Fiat_Shamir/FiatShamir.java
@@ -4,19 +4,20 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Arrays;
 
 /**
  * Created by admin on 30.10.2015.
  */
 public class FiatShamir {
     //Открытые данные
+//    private int hashLength = 160;
     private int bitSize;
     private int k;
     private BigInteger t;
     private BigInteger b[];
     private byte[] message;
     private byte[] hash;
+    private byte[] bits;
     private BigInteger[] signature = new BigInteger[2];
 
     //Секретные данные
@@ -38,13 +39,22 @@ public class FiatShamir {
     }
 
     //конструктор для проверки подписи
-    public FiatShamir(BigInteger n, BigInteger a[]) {
+    public FiatShamir(byte[] message, BigInteger n, BigInteger b[],
+                      BigInteger[] sign) {
+        this.message = message;
         this.n = n;
-        this.bitSize = n.bitLength();
+//        this.bitSize = n.bitLength();
         this.rand = new SecureRandom();
-        this.k = v.length;
-        this.v = v;
-        generateSecretKey();
+        this.b = b;
+        this.bits = sign[0].toByteArray();
+        this.t = sign[1];
+
+        byte[] temp = new byte[]{0};
+        while (bits.length < 160) {
+            bits = concat(temp, bits);
+        }
+
+        checkSign();
     }
 
     public static String byteArrayToHexString(byte[] b) {
@@ -54,18 +64,6 @@ public class FiatShamir {
                     Integer.toString((aB & 0xff) + 0x100, 16).substring(1);
         }
         return result;
-    }
-
-    public static byte[] toSHA1(byte[] convertme) {
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        assert md != null;
-//        return byteArrayToHexString(md.digest(convertme));
-        return md.digest(convertme);
     }
 
     private void generateParameters() {
@@ -83,31 +81,8 @@ public class FiatShamir {
         generateOpenKey();
         generateDS();
         System.out.println("Signature: (" + signature[0] +
-                ", " + signature[1] + ")");
-    }
-
-    private void generateDS() {
-        BigInteger mult = BigInteger.ONE;
-        for (int i = 0; i < hash.length; i++) {
-            BigInteger temp = new BigInteger(new byte[]{hash[i]});
-            mult = mult.multiply(a[i].modPow(temp, n));
-        }
-        t = r.multiply(mult);
-        t = t.mod(n);
-        signature[0] = new BigInteger(hash).abs();
-        signature[1] = t;
-    }
-
-    private void generateSecret() {
-    }
-
-    private byte[] concat(byte[] a, byte[] b) {
-        int aLen = a.length;
-        int bLen = b.length;
-        byte[] result = new byte[aLen + bLen];
-        System.arraycopy(a, 0, result, 0, aLen);
-        System.arraycopy(b, 0, result, aLen, bLen);
-        return result;
+                "(s), " + signature[1] + "(t))");
+        System.out.println("***Для проверки подписи сохраните n, открытый ключ, а также саму подпись!***");
     }
 
     private BigInteger randomNumber(boolean prime, int size) {
@@ -122,38 +97,17 @@ public class FiatShamir {
         return number;
     }
 
-    public int[] hexToBin(byte[] hex) {
-//        int[] bits = new int[bitSize * 8 + 1];
-        int[] bits = new int[160];
-        for (int i = 0; i < 160; i++) {
-            int sourceByte = 0xFF & (int) hex[i];//convert byte to unsigned int
-            int mask = 0x80;
-            for (int j = 0; j < 8; j++) {
-                int maskResult = sourceByte & mask;  // Extract the single bit
-                if (maskResult > 0) {
-                    bits[8 * i + j] = 1;
-                } else {
-                    bits[8 * i + j] = 0;  // Unnecessary since array is initiated to zero but good documentation
-                }
-                mask = mask >> 1;
-            }
-        }
-        System.out.print("Hash string in bits:   ");
-        for (int i = 0; i < bitSize; i++) {
-            System.out.print(bits[i]);
-        }
-        return bits;
-    }
-
     private void generateSecretKey() {
         byte[] concat = concat(message, u.toByteArray());
         hash = toSHA1(concat);
-        int[] bits = hexToBin(hash);
+        bits = hexToBin(hash);
+//        System.out.println(Arrays.toString(bits));
         System.out.println();
 //        System.out.println("Hash = " + Arrays.toString(hash));
-        System.out.println("Current secret key = ");
-        a = new BigInteger[hash.length];
-        for (int i = 0; i < hash.length; i++) {
+        System.out.println("Your secret key: ");
+//        System.out.println("Secret key is gonna be of " + bits.length + " numbers.");
+        a = new BigInteger[bits.length];
+        for (int i = 0; i < bits.length; i++) {
             BigInteger s_temp = randomNumber(false, bitSize);
             while (!(s_temp.gcd(n).equals(BigInteger.ONE)))
                 s_temp = randomNumber(false, bitSize);
@@ -161,6 +115,72 @@ public class FiatShamir {
             System.out.print(a[i]);
         }
         System.out.println("\n------------------");
+    }
+
+    private void generateOpenKey() {
+        b = new BigInteger[bits.length];
+//        System.out.println("Secret key is gonna be of " + bits.length + " numbers.");
+        System.out.println("Your open key is: ");
+        for (int i = 0; i < bits.length; i++) {
+            b[i] = a[i].modInverse(n).modPow(BigInteger.valueOf(2), n);
+//            b[i] = inverse(a[i], n).modPow(new BigInteger(String.valueOf(2)), n);
+            System.out.print(b[i] + ".");
+        }
+        System.out.println("\n------------------");
+    }
+
+    private void generateDS() {
+        BigInteger mult = BigInteger.ONE;
+        for (int i = 0; i < bits.length; i++) {
+            BigInteger temp = new BigInteger(String.valueOf(bits[i]));
+            mult = mult.multiply(a[i].modPow(temp, n));
+        }
+        t = r.multiply(mult);
+        t = t.mod(n);
+        signature[0] = new BigInteger(bits);
+//        System.out.println(Arrays.toString(bits));
+//        System.out.println(Arrays.toString(signature[0].toByteArray()));
+        signature[1] = t;
+    }
+
+    private void checkSign() {
+        BigInteger mult = BigInteger.ONE;
+        for (int i = 0; i < bits.length; i++) {
+            BigInteger temp = new BigInteger(String.valueOf(bits[i]));
+            mult = mult.multiply(b[i].modPow(temp, n));
+        }
+        BigInteger w = t.multiply(t).multiply(mult).mod(n);
+        byte[] concat = concat(message, w.toByteArray());
+        hash = toSHA1(concat);
+        byte[] newBits = hexToBin(hash);
+        BigInteger newSign = new BigInteger(newBits);
+        BigInteger oldSign = new BigInteger(bits);
+//        System.out.println(Arrays.toString(newBits));
+//        System.out.println(Arrays.toString(bits));
+        if (newSign.equals(oldSign))
+            System.out.println("Подпись верна.");
+        else System.out.println("Подпись не принята.");
+    }
+
+    private byte[] concat(byte[] a, byte[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+        byte[] result = new byte[aLen + bLen];
+        System.arraycopy(a, 0, result, 0, aLen);
+        System.arraycopy(b, 0, result, aLen, bLen);
+        return result;
+    }
+
+    public static byte[] toSHA1(byte[] convertme) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert md != null;
+//        return byteArrayToHexString(md.digest(convertme));
+        return md.digest(convertme);
     }
 
     public BigInteger inverse(BigInteger a, BigInteger n) {
@@ -184,11 +204,36 @@ public class FiatShamir {
         return x;
     }
 
-    private void generateOpenKey() {
-        b = new BigInteger[hash.length];
-        for (int i = 0; i < hash.length; i++) {
-            b[i] = inverse(a[i], n).modPow(new BigInteger(String.valueOf(2)), n);
+    public byte[] hexToBin(byte[] hex) {
+        byte[] res = new byte[160];
+        //array of bits is of SHA1 hash string bit length
+//        bits = new byte[160];
+//        int counter = 0;
+        for (int i = 0; i < 20; i++) {
+            int sourceByte = 0xFF & (int) hex[i];//convert byte to unsigned int
+            int mask = 0x80;
+            for (int j = 0; j < 8; j++) {
+                int maskResult = sourceByte & mask;  // Extract the single bit
+                if (maskResult > 0) {
+                    res[8 * i + j] = 1;
+                } else {
+                    res[8 * i + j] = 0;  // Unnecessary since array is initiated to zero but good documentation
+                }
+                mask = mask >> 1;
+            }
         }
+//        System.out.print("Hash string in bits:   ");
+//        for (int i = 0; i < 160; i++) {
+//            System.out.print(bits[i]);
+//        }
+        return res;
     }
 }
-
+//n
+//1928814779
+//openKey
+//921072681.1382740919.134326356.184042062.1679842819.968844826.743478837.68269175.1901430723.930218668.1746997959.526817406.1500078090.195314619.262494223.805382760.549389237.1767725187.103441546.1148695947.290610941.319853733.1259099934.181486182.522528349.237148278.969458326.928666924.232502975.378764475.1801575970.1144990071.1507938629.1409505457.1119100976.592747725.419353263.203290957.1116605099.286795950.1384370830.1768259032.1857020581.1081499152.619228143.1807120747.1228294244.426847761.66128526.1344716467.711766163.527022212.1492542121.469202450.607616290.497080333.1348399261.987223107.268360497.383402125.1864594689.394221284.338158331.330496073.284019578.746394017.651322065.1329017784.1090201811.167784181.929546546.1813650121.1584889433.777610854.452925769.1598532127.106582917.1120843943.350128077.813383643.740456590.762956239.1467355517.1769543058.808907825.1582709640.357735602.605783810.849144763.1597755986.1049701450.1483338769.1288429456.857182792.60370398.1671583505.267637105.426630013.1234818421.745197772.1535051601.927781008.1431050306.794457591.733103754.1383265629.523382169.474942311.239717355.985646286.1428076879.556422844.90818982.1249262861.1027947361.89169490.369195486.592251671.987672177.1180168604.1076147581.1309654601.544556440.704299574.1811703076.1368790914.1672393884.1850960697.240282989.1782937989.1535923227.489079947.101618023.1881146352.551139328.1706958764.2397912.1662606083.133177266.743134918.924315079.1666792398.752293246.517805672.1581838451.1208938271.957990853.703574450.1125096630.262844575.1214745920.22583548.1314391872.1186720310.1590713611.1552049031.1120264946.1619371084.1210273992.556034279
+//s
+//81311975136318641314402609712165089885923552589828905572746519709274178906557041507278220203237773191053567177790507870682272611395185140130694344782560408046667664176603123635119884714305334249895058999709321218782922296368560996907170881328638532274412047547173645569576251367523006216059942651219303894804677939119212846799575560051904884650764286707881546217186387354068692500481(s), 974677880(t)
+//t
+// 974677880
--- src/Fiat_Shamir/Main.java
+++ src/Fiat_Shamir/Main.java
@@ -5,12 +5,20 @@ import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 /**
  * Created by admin on 30.10.2015.
  */
 public class Main {
+    private static BigInteger[] parseOpenKey(String raw){
+        String[] prep = raw.split("[.]");
+        BigInteger[] res = new BigInteger[prep.length];
+        for (int i = 0; i < prep.length; i++) {
+            res[i] = new BigInteger(prep[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -25,7 +33,7 @@ public class Main {
                 bw.write("Введите длину чисел в битах: \n");
                 bw.flush();
                 int bitLength = Integer.parseInt(br.readLine());
-                System.out.println(bitLength);
+//                System.out.println(bitLength);
                 Path path = Paths.get(source);
                 byte[] message = Files.readAllBytes(path);
                 FiatShamir fs = new FiatShamir(bitLength, message);
@@ -33,16 +41,30 @@ public class Main {
                 break;
             case "check":
                 bw.write("Введите путь к подписанному файлу: \n");
+                bw.flush();
                 source = br.readLine();
                 path = Paths.get(source);
                 message = Files.readAllBytes(path);
+                bw.write("Ваш открытый ключ: \n");
+                bw.flush();
+                BigInteger[] openKey = parseOpenKey(br.readLine());
+                bw.write("Введите n, использованное при подписывании: ");
+                bw.flush();
+                BigInteger n = new BigInteger(br.readLine());
                 bw.write("Ваша подпись: \n");
+                bw.flush();
                 BigInteger[] signature = new BigInteger[2];
                 bw.write("s = \n");
-                BigInteger s = new BigInteger(br.readLine());
+                bw.flush();
+                signature[0] = new BigInteger(br.readLine()); //s
                 bw.write("t = \n");
-                BigInteger t = new BigInteger(br.readLine());
-
+                bw.flush();
+                signature[1] = new BigInteger(br.readLine()); //t
+                fs = new FiatShamir(message, n, openKey, signature);
+                break;
+            default:
+                System.err.println("Такой режим неизвестен.");
+                break;
 
         }
 
